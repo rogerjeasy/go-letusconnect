4ca4a7a776d27a81a140da62f2213fd8fe638c9b
rogerjeasy <rogerjeasy@gmail.com>
2025-01-31 22:46:08 +0100

    added more mapper

--- mappers/user_service_test_mapper.go
+++ mappers/user_service_test_mapper.go
@@ -140,3 +140,72 @@ func TestMapUserBackendToFrontend(t *testing.T) {
 	result := MapUserBackendToFrontend(backendUser)
 	assert.Equal(t, expected, result)
 }
+
+func TestMapFrontendToUser(t *testing.T) {
+	data := map[string]interface{}{
+		"uid":              "123",
+		"username":         "testuser",
+		"firstName":        "John",
+		"lastName":         "Doe",
+		"email":            "john.doe@example.com",
+		"phoneNumber":      "1234567890",
+		"profilePicture":   "profile.jpg",
+		"bio":              "Hello, I'm John!",
+		"role":             []interface{}{"user"},
+		"graduationYear":   2020,
+		"currentJobTitle":  "Software Engineer",
+		"areasOfExpertise": []interface{}{"Go", "JavaScript"},
+		"interests":        []interface{}{"Programming", "Reading"},
+		"lookingForMentor": true,
+		"willingToMentor":  false,
+		"connectionsMade":  10,
+		"accountCreatedAt": "2020-01-01",
+		"isActive":         true,
+		"isVerified":       true,
+		"password":         "password",
+		"program":          "Computer Science",
+		"dateOfBirth":      "1990-01-01",
+		"phoneCode":        "+1",
+		"languages":        []interface{}{"English", "Spanish"},
+		"skills":           []interface{}{"Go", "JavaScript"},
+		"certifications":   []interface{}{"AWS Certified"},
+		"projects":         []interface{}{"Project 1", "Project 2"},
+		"isOnline":         true,
+		"isPrivate":        false,
+	}
+
+	expected := models.User{
+		UID:              "123",
+		Username:         "testuser",
+		FirstName:        "John",
+		LastName:         "Doe",
+		Email:            "john.doe@example.com",
+		PhoneNumber:      "1234567890",
+		ProfilePicture:   "profile.jpg",
+		Bio:              "Hello, I'm John!",
+		Role:             []string{"user"},
+		GraduationYear:   2020,
+		CurrentJobTitle:  "Software Engineer",
+		AreasOfExpertise: []string{"Go", "JavaScript"},
+		Interests:        []string{"Programming", "Reading"},
+		LookingForMentor: true,
+		WillingToMentor:  false,
+		ConnectionsMade:  10,
+		AccountCreatedAt: "2020-01-01",
+		IsActive:         true,
+		IsVerified:       true,
+		Password:         "password",
+		Program:          "Computer Science",
+		DateOfBirth:      "1990-01-01",
+		PhoneCode:        "+1",
+		Languages:        []string{"English", "Spanish"},
+		Skills:           []string{"Go", "JavaScript"},
+		Certifications:   []string{"AWS Certified"},
+		Projects:         []string{"Project 1", "Project 2"},
+		IsOnline:         true,
+		IsPrivate:        false,
+	}
+
+	result := MapFrontendToUser(data)
+	assert.Equal(t, expected, result)
+}
